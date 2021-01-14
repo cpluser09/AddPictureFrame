34830d767a333eded31cdaae12fbeeaa6c7262cd
cpluser <enjoyimage@163.com>
2021-01-14 23:25:13 +0800

    remove debug code

--- prepare_print_file.py
+++ prepare_print_file.py
@@ -1,5 +1,5 @@
 from add_frame import *
 
 if __name__ == "__main__":
-    #prepare_print(sys.argv[1])
-    prepare_print("/Users/junlin/myPhoto/yanself/2021Q1/print/01")
+    prepare_print(sys.argv[1])
+    #prepare_print("/Users/junlin/myPhoto/yanself/2021Q1/print/01")
